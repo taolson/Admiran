62c2761db03d3731bee39344e852ceb51ef0f195
Tim Olson <tim.olson.mail@gmail.com>
2025-02-10 16:08:44 -0600

    minor modifications

--- tools/dumpX2.m
+++ tools/dumpX2.m
@@ -17,24 +17,16 @@
 %import "../compiler/serialize"
 %import "../compiler/tokenizer"
 
-|| turn an excpt * into an io *
-io_excpt :: excpt * -> io *
-io_excpt (Left errs)        = io_mapM_ (errStrLn . showException) errs  >> exit 1       || print the errors and exit
-io_excpt (Right (r, warns)) = io_mapM_ (errStrLn . showException) warns >> io_pure r    || print the warnings and continue with the result
-
 deserialize :: string -> io module
 deserialize fn
     = mtimeFile fx >>= exists
       where
-        fm         = withSuffix "m" fn
         fx         = withSuffix "x2" fn
         doDeserial = io_pure . fst . deserialModule
 
         exists tsm
-            = io_excpt (ex_error err emptyLocInfo), if tsm < 0
-            = readFile fx >>= doDeserial,           otherwise
-              where
-                err = Info $ fm ++ " required, but not found"
+            = error (fx ++ " required, but not found"), if tsm < 0
+            = readFile fx >>= doDeserial,               otherwise
 
 main :: io ()
 main
